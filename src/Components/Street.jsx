2a17937a4dd58b13b87bb94b274ca9b2e18d9fcb
farrelekaputra <128167479+farrelekaputra@users.noreply.github.com>
2025-12-30 22:21:40 +0700

    ...

--- src/Components/Street.jsx
+++ src/Components/Street.jsx
@@ -11,15 +11,15 @@ import street9 from "../assets/street/1766854790066.jpg"
 const StreetPhoto = () => {
     return (
         <ul className="mainClass flex flex-col w-full gap-4">
-            <li><img src={street1} alt="Street 1" /></li>
-            <li><img src={street2} alt="Street 2" /></li>
-            <li><img src={street3} alt="Street 3" /></li>
-            <li><img src={street4} alt="Street 4" /></li>
-            <li><img src={street5} alt="Street 5" /></li>
-            <li><img src={street6} alt="Street 6" /></li>
-            <li><img src={street7} alt="Street 7" /></li>
-            <li><img src={street8} alt="Street 8" /></li>
-            <li><img src={street9} alt="Street 9" /></li>
+            <li><img src={street1} classname="md:w-1/2" alt="Street 1" /></li>
+            <li><img src={street2} classname="md:w-1/2" alt="Street 2" /></li>
+            <li><img src={street3} classname="md:w-1/2" alt="Street 3" /></li>
+            <li><img src={street4} classname="md:w-1/2" alt="Street 4" /></li>
+            <li><img src={street5} classname="md:w-1/2" alt="Street 5" /></li>
+            <li><img src={street6} classname="md:w-1/2" alt="Street 6" /></li>
+            <li><img src={street7} classname="md:w-1/2" alt="Street 7" /></li>
+            <li><img src={street8} classname="md:w-1/2" alt="Street 8" /></li>
+            <li><img src={street9} classname="md:w-1/2" alt="Street 9" /></li>
         </ul>
     )
 }
