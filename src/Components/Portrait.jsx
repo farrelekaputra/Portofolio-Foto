450d935b07f948d29caaedaccb8bdf6c944aa7e4
farrelekaputra <128167479+farrelekaputra@users.noreply.github.com>
2026-03-30 21:03:57 +0700

    tambah foto

--- src/Components/Portrait.jsx
+++ src/Components/Portrait.jsx
@@ -4,6 +4,10 @@ import Portrait3 from "../assets/portrait/3.webp"
 import Portrait4 from "../assets/portrait/4.webp"
 import Portrait5 from "../assets/portrait/5.webp"
 import Portrait6 from "../assets/portrait/6.webp"
+import Portrait7 from "../assets/portrait/7.webp"
+import Portrait8 from "../assets/portrait/8.webp"
+import Portrait9 from "../assets/portrait/9.webp"
+import Portrait10 from "../assets/portrait/10.webp"
 
 const PortraitPhoto = () => {
     return (
@@ -12,8 +16,10 @@ const PortraitPhoto = () => {
                 <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait2} className="w-full h-full object-cover" alt="Portrait 2" /></li>
                 <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait3} className="w-full h-full object-cover" alt="Portrait 3" /></li>
                 <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait4} className="w-full h-full object-cover" alt="Portrait 4" /></li>
-                <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait5} className="w-full h-full object-cover" alt="Portrait 5" /></li>
-                <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait6} className="w-full h-full object-cover" alt="Portrait 6" /></li>
+                <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait7} className="w-full h-full object-cover" alt="Portrait 5" /></li>
+                <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait8} className="w-full h-full object-cover" alt="Portrait 6" /></li>
+                <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait9} className="w-full h-full object-cover" alt="Portrait 6" /></li>
+                <li className="w-full md:w-[calc(31%-0.31rem)] aspect-[2/3] overflow-hidden"><img src={Portrait10} className="w-full h-full object-cover" alt="Portrait 6" /></li>
             </ul>
             )
         }
